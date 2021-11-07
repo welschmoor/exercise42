e0c9dee1824c3c1e7d60fc0d28b82f41b8bd9384
Johannes Welsch <welschmoor@gmail.com>
2021-11-07 13:08:11 +0100

    finally got it working...

--- controller/bloglists.js
+++
@@ -1,2 +0,0 @@
-const notesRouter = require('express').Router()
-const Note = require('../models/note')
--- index.js
+++ index.js
@@ -1,40 +1,10 @@
-require('dotenv').config()	
+const app = require('./app') // the actual Express application
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const Blog = require('./models/bloglist')
-const url = process.env.MONGODB_URI
-const PORT = process.env.PORT 
+const server = http.createServer(app)
 
-
-const mongoUrl = 'mongodb+srv://user:hurensohn@cluster0.rtegy.mongodb.net/bloglist?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
