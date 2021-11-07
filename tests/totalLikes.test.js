7a3d00a446d9b6b44bb0ba5e3b29308c63cc6d2c
Mike Lambo <welschmoor@gmail.com>
2021-11-08 00:46:57 +0100

    added more tests 4a

--- tests/totalLikes.test.js
+++ tests/totalLikes.test.js
@@ -3,30 +3,37 @@ const totalLikes = require('../utils/list_helper').totalLikes
 
 describe('totalLikes', () => {
 
+
   // simple data
   test('totalLikes test', () => {
-
     const testdata = [
       {likes: 30},
       {likes: 1},
       {likes: 3}
     ]
-    
     const result = totalLikes(testdata)
     expect(result).toBe(34)
   })
 
+
   // data missing
   test('totalLikes data missing test', () => {
-
     const testdata = [
       {likes: 30},
       {name: "Mike"},
       {likes: 3}
     ]
-    
     const result = totalLikes(testdata)
     expect(result).toBe(33)
   })
+  
 
+  // only one post 
+  test('totalLikes test', () => {
+    const testdata = [
+      {likes: 12},
+    ]
+    const result = totalLikes(testdata)
+    expect(result).toBe(12)
+  })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,14 +1,28 @@
+///////////////////////////
+
 const dummy = (blogs) => {
   return 1
 }
 
-const totalLikes = (blogslist) => {
-  return blogslist.reduce( (acc, each) => {
+
+const totalLikes = (bloglist) => {
+  return bloglist.reduce( (acc, each) => {
     return each.likes ? acc + each.likes : acc
   }, 0)
 }
 
+
+const favoriteBlog = (bloglist) => {
+  const highestLikes = bloglist.reduce( (acc, each) => {
+    return each.likes > acc ?  each.likes : acc;
+   }, 0)
+  return bloglist.find(each => each.likes === highestLikes )
+}
+
+
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
